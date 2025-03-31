06c606fac7d9fc5467186b3f3bb51c2023ea0215
PullUpjahs <s27558675@gmail.com>
2025-03-31 18:16:56 +0900

    Update login.js

--- login.js
+++ login.js
@@ -15,9 +15,9 @@ const students = [
       message2: "성적역사 - 국어24년6월 고1 점수:86점/2등급", 
       downloadLink: "https://horaeng.com/storage/2024%EB%85%84-6%EC%9B%94-%EA%B3%A01-%EB%AA%A8%EC%9D%98%EA%B3%A0%EC%82%AC-%ED%86%B5%ED%95%A9%EA%B3%BC%ED%95%99-%ED%95%B4%EC%84%A4.pdf" },
 
-    { id: "홍나연1", password: "2222", score: "86", grade: "2등급",
-      message1: "좋은 성적이에요! 꾸준히 노력하세요!", 
-      message2: "성적역사 - Na", 
+    { id: "홍나연1", password: "2222", score: "60", grade: "4등급",
+      message1: "괜찮습니다, 풀업 동아리에서 꾸준한 연습과 피드백을 받으면서 같이 성적을 끌어올려봅시다! 꾸준히 노력하세요!", 
+      message2: "성적역사 - 수학23년6월 고1 점수:86점/2등급", 
       downloadLink: "https://horaeng.com/storage/2021%ED%95%99%EB%85%84%EB%8F%84-6%EC%9B%94-%EA%B3%A01-%EB%AA%A8%EC%9D%98%EA%B3%A0%EC%82%AC-%EC%98%81%EC%96%B4-%ED%95%B4%EC%84%A4.pdf" },
 
     { id: "홍수민1", password: "3333", score: "82", grade: "2등급",
